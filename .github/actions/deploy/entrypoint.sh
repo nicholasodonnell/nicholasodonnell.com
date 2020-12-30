da54964caec7cc2f9cad6529582e5cd3b822b2f2
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2020-12-30 14:15:19 -0500

    Fix

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -5,7 +5,7 @@ git config --global user.email "nicholas@nicholasodonnell.com"
 git config --global user.name "Nicholas O'Donnell"
 
 # clone gh-pages branch
-git clone --branch gh-pages https://$1@github.com/$GITHUB_REPOSITORY.com.git /gh-pages
+git clone --branch gh-pages https://$1@github.com/$GITHUB_REPOSITORY.git /gh-pages
 
 # update gh-pages branch with changes
 echo "synching /public directory"
