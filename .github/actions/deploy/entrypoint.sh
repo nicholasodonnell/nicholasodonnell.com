2102108ff90785bd4879b9ede4445998cc21900b
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2020-12-30 14:11:29 -0500

    Cleanup actions

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -5,11 +5,17 @@ git config --global user.email "nicholas@nicholasodonnell.com"
 git config --global user.name "Nicholas O'Donnell"
 
 # clone gh-pages branch
-git clone --branch gh-pages https://$1@github.com/nicholasodonnell/nicholasodonnell.com.git /gh-pages
+git clone --branch gh-pages https://$1@github.com/$GITHUB_REPOSITORY.com.git /gh-pages
 
 # update gh-pages branch with changes
 echo "synching /public directory"
+rm -rf /gh-pages/*
 mv -f $GITHUB_WORKSPACE/public/* /gh-pages
 
+# set working dir
+cd /gh-pages
+
 # push gh-pages branch
-sh -c "cd /gh-pages && git add . && git commit --allow-empty -m \"Publish $GITHUB_SHA\" && git push --force https://$1@github.com/nicholasodonnell/nicholasodonnell.com.git gh-pages"
+git add --all
+git commit --allow-empty --message="Publish $GITHUB_SHA"
+git push --force https://$1@github.com/$GITHUB_REPOSITORY.git gh-pages
